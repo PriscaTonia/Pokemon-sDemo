50965ad346bd8b6010b37dab7340b5fd653d20e8
PriscaTonia <priscaebube@gmail.com>
2020-11-17 12:54:12 +0100

    I successfully reached out to an external REST API to retrieve articles to post in my home page

--- src/components/home.js
+++ src/components/home.js
@@ -3,25 +3,33 @@ import axios from 'axios'
 
 class Home extends Component {
     state ={
-        posts: [ ]
+        articles: [ ]
     }
     componentDidMount(){
-        axios.get('https://jsonplaceholder.typicode.com/posts')
+        axios.get('http://newsapi.org/v2/top-headlines?country=ng&category=technology&apiKey=3a6fdb5a481d4c859dc8b9c3455a7641')
         .then(res =>{
             this.setState({
-                posts: res.data.slice(0, 10)
+               articles: res.data.articles.slice(0, 10)
             })
+            console.log(res)
+           console.log(this.state)
         })
     }
     render(){
-        const {posts} = this.state;
-        const postList = posts.length ? (
-            posts.map(post => {
+        const {articles} = this.state;
+        const articleList = articles.length ? (
+            articles.map(article => {
                 return(
-                    <div key={post.id} className='card my-3'>
+                    <div key={article.id} className='card my-3'>
                         <div className='content card-block'>
-                            <h5 className='title card-title'>{post.title}</h5>
-                            <p className='card-text'>{post.body}</p>
+                            <h5 className='title card-title'>{article.title}</h5>
+                            <p className='card-text'>{article.description}</p>
+                            <p className='text-center'>
+                                By: {
+                                    (article.author !== "" && article.author !== null) ? (article.author) : ('anonymous')
+                                 }</p>
+                            <a className='card-link' href={article.url}>See article</a>
+                            
                         </div>
                     </div>
                 )
@@ -33,7 +41,7 @@ class Home extends Component {
             <div className='container'>
                 <h4 className='text-center py-2'>Home</h4>
                 <div className='container'>
-                    <p> {postList} </p>
+                    <p> {articleList} </p>
                 </div>
             </div>
         )
